0ca2a9cec7be44aad0b4cab2951e4f6540517371
khamiltonuk <krisham87@hotmail.com>
2018-05-09 23:04:07 +0200

    change font size on input press

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./Screen.css";
 
 export default function Screen(props) {
+  const calculatedFontSize = props.displayValue.length > 10 ? "1em" : "2em";
+  console.log(calculatedFontSize);
   return (
     <div className="screen">
-      <input onChange={e => props.handleChange(e)} value={props.displayValue} />
+      <input
+        style={{ fontSize: calculatedFontSize }}
+        onChange={e => props.handleChange(e)}
+        value={props.displayValue}
+      />
       <button type="submit" onClick={() => props.enterNumber()}>
         Input number
       </button>
