707bd1d774537a84de5567ccc122b2b4fd3a3aab
khamiltonuk <krisham87@hotmail.com>
2018-05-09 10:09:03 +0200

    first few functions

--- src/helpers/helper.spec.js
+++ src/helpers/helper.spec.js
@@ -1,7 +1,29 @@
-function squareAll(numbers) {
-  return numbers.map(num => num * num);
+import { map, reduce } from "lodash/fp";
+
+const squareAll = map(num => num * num);
+
+function addAll(arr) {
+  return arr.reduce((a, b) => a + b);
 }
 
-it("renders without crashing", () => {
-  expect(squareAll([1, 2, 3, 4])).toEqual([1, 4, 9, 16]);
+function averageAll(arr) {
+  return addAll(arr) / arr.length;
+}
+
+describe("squareAll", () => {
+  test("sqaures all numbers in an array", () => {
+    expect(squareAll([1, 2, 3, 4])).toEqual([1, 4, 9, 16]);
+  });
+});
+
+describe("addAll", () => {
+  test("adds all numbers in an array", () => {
+    expect(addAll([1, 2, 3, 4])).toEqual(10);
+  });
+});
+
+describe("averageAll", () => {
+  test("should finds the mean of all the numbers", () => {
+    expect(averageAll([1, 2, 3, 4])).toEqual(2.5);
+  });
 });
