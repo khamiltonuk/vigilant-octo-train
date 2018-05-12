20a153b6557591dc29d6de99919f804037e29af9
khamiltonuk <krisham87@hotmail.com>
2018-05-12 12:44:41 +0200

    add animation to add numbers

--- src/components/Histroy/History.jsx
+++ src/components/Histroy/History.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import FlipMove from "react-flip-move";
 
 import "./History.css";
 
 export default function History(props) {
   const { removeValue, values } = props;
+  const historyItem = values.map((el, i) => {
+    return (
+      <li key={i}>
+        {el} <button onClick={() => removeValue(i)}>&#10006;</button>
+      </li>
+    );
+  });
   return (
     <ul className="history group">
-      {values.map((el, i) => {
-        return (
-          <li key={i}>
-            {el} <button onClick={() => removeValue(i)}>&#10006;</button>
-          </li>
-        );
-      })}
+      <FlipMove duration={250} easing="ease-out">
+        {historyItem}
+      </FlipMove>
     </ul>
   );
 }
