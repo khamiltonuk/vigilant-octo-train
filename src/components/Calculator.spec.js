6e635b6e610ab356dd00184c6efae4e50429a361
khamiltonuk <krisham87@hotmail.com>
2018-06-07 10:01:38 +0200

    snapshot for Calculator

---
+++ src/components/Calculator.spec.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import Calculator from "./Calculator";
+
+describe("Calculator", () => {
+  test("should render correctly", () => {
+    const wrapper = shallow(<Calculator />);
+    console.log(wrapper.debug());
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
